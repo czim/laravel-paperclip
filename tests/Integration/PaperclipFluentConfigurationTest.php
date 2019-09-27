60da50b969a26e63db09358ec4ca3deccad2796d
Coen Zimmerman <coen@pxlwidgets.com>
2019-09-27 10:46:36 +0200

    Added a test specifically for a PNG file

--- tests/Integration/PaperclipFluentConfigurationTest.php
+++ tests/Integration/PaperclipFluentConfigurationTest.php
@@ -72,6 +72,55 @@ class PaperclipFluentConfigurationTest extends ProvisionedTestCase
     /**
      * @test
      */
+    function it_processes_and_stores_a_new_png_file_with_fluent_variant_steps_configuration()
+    {
+        $model = $this->getTestModelWithAttachmentConfig([
+            'variants' => [
+                'test' => [
+                    AutoOrientStep::make()->quiet(),
+                    ResizeStep::make()->width(100)->height(100)
+                        ->ignoreRatio()
+                        ->convertOptions(['quality' => 90]),
+                ],
+            ],
+        ]);
+
+        $model->attachment = new SplFileInfo($this->getTestFilePath('picture.png'));
+        $model->save();
+
+        $processedFilePath = $this->getUploadedAttachmentPath($model, 'picture.png');
+
+        static::assertInstanceOf(Attachment::class, $model->attachment);
+        static::assertEquals('picture.png', $model->attachment_file_name);
+        static::assertFileExists($processedFilePath, 'File was not stored');
+
+        static::assertEquals(
+            [
+                'variants' => [
+                    'test' => [
+                        'resize'      => [
+                            'dimensions'     => '100x100!',
+                            'convertOptions' => [
+                                'quality' => 90,
+                            ],
+                        ],
+                        'auto-orient' => [
+                            'quiet' => true,
+                        ],
+                    ],
+                ],
+            ],
+            $model->attachment->getNormalizedConfig()
+        );
+
+        if (file_exists($processedFilePath)) {
+            unlink($processedFilePath);
+        }
+    }
+
+    /**
+     * @test
+     */
     function it_processes_and_stores_a_new_file_with_fluent_variant_configuration()
     {
         $model = $this->getTestModelWithAttachmentConfig([
